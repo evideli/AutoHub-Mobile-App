d03ae7dbeee41a07d5243b41b74b471618e472cb
Evdoxia Deligianni <evideli92@gmail.com>
2020-03-21 21:04:28 +0200

    week1 simeiwseis

--- app/src/main/java/gr/teicm/msc/autohub/MainActivity.java
+++ app/src/main/java/gr/teicm/msc/autohub/MainActivity.java
@@ -1,10 +1,35 @@
 package gr.teicm.msc.autohub;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.MenuInflater;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_exit:
+                finish();
+                return true;
+            case R.id.menu_settings:
+                Toast.makeText(this, "Under construction", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.home_page, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

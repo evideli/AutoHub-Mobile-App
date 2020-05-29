3f52c8383cdb1a631ee79b8d1e0b13a6e6b5bcf4
evideli <evideli92@gmail.com>
2020-05-29 21:18:33 +0300

    Added tablet and landscape layouts and proguard optimizations

--- app/src/main/java/gr/teicm/msc/autohub/CarDetails.java
+++ app/src/main/java/gr/teicm/msc/autohub/CarDetails.java
@@ -18,8 +18,13 @@ import gr.teicm.msc.autohub.classes.ImageLoader;
 public class CarDetails extends AppCompatActivity {
 
     TextView textViewTitle;
-    TextView textViewAuthor;
-    TextView textViewGenre;
+    TextView textViewBrand;
+    TextView textViewDescription;
+    TextView textViewType;
+    TextView textViewYear;
+    TextView textViewFuel;
+    TextView textViewPrice;
+    TextView textViewTransmission;
     Button buttonVisitWebsite;
     ImageView imageViewCover;
     ImageLoader imageLoader;
@@ -27,9 +32,14 @@ public class CarDetails extends AppCompatActivity {
     HashMap<String, Object> car = null;
 
     private void findViews() {
-        textViewTitle = (TextView) findViewById(R.id.car_details_car);
-        textViewAuthor = (TextView) findViewById(R.id.car_details_brand);
-        textViewGenre = (TextView) findViewById(R.id.car_details_type);
+        textViewTitle = (TextView) findViewById(R.id.car_details_model);
+        textViewBrand = (TextView) findViewById(R.id.car_details_brand);
+        textViewDescription = (TextView) findViewById(R.id.car_details_description);
+        textViewType = (TextView) findViewById(R.id.car_details_type);
+        textViewYear = (TextView) findViewById(R.id.car_details_year);
+        textViewFuel = (TextView) findViewById(R.id.car_details_fuel);
+        textViewPrice = (TextView) findViewById(R.id.car_details_price);
+        textViewTransmission = (TextView) findViewById(R.id.car_details_transmission);
         buttonVisitWebsite = (Button) findViewById(R.id.buttonVisitWebsite);
         imageViewCover = (ImageView) findViewById(R.id.imageViewCover);
     }
@@ -38,10 +48,6 @@ public class CarDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_details);
-
-        //Animation when this Activity appears
-
-
         findViews();
 
         Intent intent = getIntent();
@@ -51,10 +57,22 @@ public class CarDetails extends AppCompatActivity {
         String carName = (String) car.get(DataStore.KEY_CAR);
         String carBrand = (String) car.get(DataStore.KEY_BRANDNAME);
         String carType = (String) car.get(DataStore.KEY_TYPENAME);
+        String carYear = (String) car.get(DataStore.KEY_YEAR);
+        String carFuel = (String) car.get(DataStore.KEY_FUEL);
+        String carPrice = (String) car.get(DataStore.KEY_PRICE);
+        String carTransmission = (String) car.get(DataStore.KEY_TRANSMISSON);
+        String carDescription = (String) car.get(DataStore.KEY_DESCRIPTION);
         String carCoverUrl = (String) car.get(DataStore.KEY_COVERURL);
+
         textViewTitle.setText(carName);
-        textViewAuthor.setText(carBrand);
-        textViewGenre.setText(carType);
+        textViewType.setText(carType);
+        textViewBrand.setText(carBrand);
+        textViewDescription.setText(carDescription);
+        textViewYear.setText(carYear);
+        textViewFuel.setText(carFuel);
+        textViewPrice.setText(carPrice);
+        textViewTransmission.setText(carTransmission);
+
         imageLoader = new ImageLoader(this);
         imageLoader.DisplayImage(carCoverUrl, imageViewCover);
 
@@ -67,5 +85,4 @@ public class CarDetails extends AppCompatActivity {
             }
         });
     }
-
 }
